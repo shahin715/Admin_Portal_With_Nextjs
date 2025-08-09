c669a5df40713fd09be10bcbc64d6d6a8f7c28b5
shahin715 <shahinahmed7165@gmail.com>
2025-08-10 03:30:38 +0600

    update

--- src/app/components/summaryCards/SummaryCards.jsx
+++ src/app/components/summaryCards/SummaryCards.jsx
@@ -12,6 +12,7 @@ export default function SummaryCards() {
 
 useEffect(() => {
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/summary`;
+
   console.log("API URL: ", apiUrl); // Log the URL being requested
   axios
     .get(apiUrl)
